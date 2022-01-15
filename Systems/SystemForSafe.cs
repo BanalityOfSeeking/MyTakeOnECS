376fac17691d40e2733c51afd076f2ef5f99f370
Ryan April <ryan.april@live.com>
2022-01-15 11:36:44 -0500

    Safe vs Unsafe : more safe code.

---
+++ Systems/SystemForSafe.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.HighPerformance.Helpers;
+
+namespace BonesOfTheFallen.Services
+{
+    // clear advantages:
+    // Less code ~ Parallel by default
+    // ** I should replace parallel and try concurrent by default. **
+
+    // disadvantage garbage ToArray()..
+    public sealed class SystemForSafe<T> where T : struct, IRefAction<T>
+    {
+        public SystemForSafe<T> TransitionInPlaceSafe<PStruct>(PStruct @struct) where PStruct : struct, IRefAction<T>
+        {
+            ParallelHelper.ForEach<T, PStruct>(ComponentCacheSafe<T>.Cache.ToArray(), @struct);
+            return this;
+        }
+    }    
+}
--- Systems/SystemForUnsafe.cs
+++ Systems/SystemForUnsafe.cs
@@ -8,7 +8,6 @@ namespace BonesOfTheFallen.Services
         // requires something I can pass in a ptr to memory and bounds,
         // then chunk it out for tasks to be queued.. a ref struct to handle it that with no garbage.
 
-
         public SystemForUnsafe<T> TransitionInPlaceUnsafe<PStruct>(PStruct @struct) where PStruct : struct, IRefAction<T>
         {
             var cache = ComponentSystemsUnsafe<T>.GetCache();
@@ -24,17 +23,4 @@ namespace BonesOfTheFallen.Services
             return this;
         }
     }
-    // clear advantages:
-    // Less code ~ Parallel by default
-    // ** I should replace parallel and try concurrent by default. **
-
-    // disadvantage garbage ToArray()..
-    public sealed class SystemForSafe<T> where T : struct, IRefAction<T>
-    {
-        public SystemForSafe<T> TransitionInPlaceSafe<PStruct>(PStruct @struct) where PStruct : struct, IRefAction<T>
-        {
-            ParallelHelper.ForEach<T, PStruct>(ComponentCacheSafe<T>.Cache.ToArray(), @struct);
-            return this;
-        }
-    }    
 }
